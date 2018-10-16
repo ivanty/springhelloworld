3e9377648194a5622d49d7c37d0fc5605228aa70
1022 <786427910@qq.com>
2018-10-16 18:38:32 +0800

    spring aware的测试

---
+++ src/main/java/com/xue/aware/AwareConfig.java
@@ -0,0 +1,9 @@
+package com.xue.aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.xue.aware")
+public class AwareConfig {
+}
